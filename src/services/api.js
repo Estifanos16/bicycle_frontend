ce7a58170ac2f9cc86c928ad657b3d7c3843242a
Estifanos16 <219389533+Estifanos16@users.noreply.github.com>
2026-05-09 07:05:13 -0700

    Add the backend url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://bicycle-backend16.onrender.com/api',
 });
 
 // Set token for protected routes
